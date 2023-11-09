86cc6e9c766a2f1d58e24cfae324b8813666b79f
Jimmy Hu <jimmyhu@ezcon.com.tw>
2023-11-09 16:02:35 +0800

    change .sh echo

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 START=$(date +%s)
 
-for i in {1..10}
+for i in {1..50}
 do
   pnpm vitest --run
 done
 
 END=$(date +%s)
 DIFF=$(( $END - $START ))
-echo "Jest tests took $DIFF seconds"
+echo "Vitest tests took $DIFF seconds"
