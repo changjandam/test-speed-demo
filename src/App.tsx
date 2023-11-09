88311da0d916b82c9d516901aa70e524302fb795
Jimmy Hu <jimmyhu@ezcon.com.tw>
2023-11-09 10:26:51 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import styles from './App.module.scss';
+
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const formSchema = z.object({
+  email: z.string().email('Invalid email'),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors: { email: emailError },
+    },
+  } = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <div className={styles.app}>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <input
+          className={styles.input}
+          type='email'
+          placeholder='enter email'
+          {...register('email')}
+        />
+        <p className={styles.errorMessage}>{emailError?.message}</p>
+        <button
+          type='submit'
+          className={styles.submitButton}
+          disabled={!!emailError}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
