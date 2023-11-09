3f280a9a565ea6a1f037c3b5e77b1fec0264f9e8
Jimmy Hu <jimmyhu@ezcon.com.tw>
2023-11-09 14:19:36 +0800

    done

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,30 @@
-import { screen, render } from '@testing-library/react';
 import App from '../App';
 
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 describe('App', () => {
-  it('renders without crashing', () => {
+  test('renders App component', () => {
     render(<App />);
-    expect(screen.getByText(''))
+    screen.getByRole('textbox');
+    screen.getByRole('button', { name: /submit/i });
+  });
+});
+
+describe('App behavior', () => {
+  test('shows error message when email is invalid', async () => {
+    render(<App />);
+    const input = screen.getByRole('textbox');
+    const user = userEvent.setup();
+    await user.type(input, 'test');
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+    expect(submitButton).toBeDisabled();
+    screen.getByText(/invalid email/i);
+    await user.type(input, '@demo.com');
+    expect(submitButton).toBeEnabled();
+    expect(screen.queryByText(/invalid email/i)).not.toBeInTheDocument();
+    const spy = vi.spyOn(console, 'log');
+    await user.click(submitButton);
+    expect(spy).toHaveBeenCalledWith({ email: 'test@demo.com' });
   });
 });
